e9c96660b30eb2e8d2275d28f32c57756ce2e263
north_h <1635273401@qq.com>
2023-09-15 00:41:10 +0800

    修改

--- Codeforces_-_Codeforces_Round_719_(Div_3)/F1_Guess_the_K-th_Zero_(Easy_version).cpp
+++ Codeforces_-_Codeforces_Round_719_(Div_3)/F1_Guess_the_K-th_Zero_(Easy_version).cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * ==================================================================================
  * Author:  north_h
  * Time:    2023-09-14 15:05:36
@@ -10,7 +10,7 @@
  * TimeL:   1000 ms
  * ==================================================================================
  */
- 
+
 #pragma GCC optimize("Ofast")
 
 #include<bits/stdc++.h>
@@ -20,7 +20,7 @@
 #define met_1(a) memset(a,-1,sizeof a)
 #define met_x(a) memset(a,0x3f,sizeof a)
 #define mpy(a, b) memcopy(a,sizeof b,b)
-#define ll long long
+#define int long long
 #define ld long double
 #define ull unsigned long long
 #define fi first
@@ -32,7 +32,7 @@
 #define ALL(a) a.begin(),a.end()
 #define rALL(a) a.rbegin(),a.rend()
 #define int128 __int128
-#define endl '\n'
+// #define endl '\n'
 const int N = 10010;
 const int M = 1910;
 const int MOD = 98244353;
@@ -41,12 +41,26 @@ const int INF = 0x3f3f3f3f;
 
 using namespace std;
 
-void solve() {}
+void solve() {
+    int n, t, k;
+    cin >> n >> t >> k;
+    int l = 1, r = n, ans;
+    while(l <= r) {
+        int mid = l + r >> 1;
+        cout << "? " << l << " " << mid << endl;
+        int x;
+        cin >> x;
+        int res = mid - l + 1 - x;
+        if(res < k)l = mid + 1, k -= res;
+        else r = mid - 1, ans = mid;
+    }
+    cout << "! " << l << endl;
+}
 
 int32_t main() {
     IOS;
     int h_h = 1;
-    cin >> h_h;
+    // cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
