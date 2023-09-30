41b2749bc8d3ca2b9d965e49b3ef7a2fc1947449
north_h <1635273401@qq.com>
2023-09-30 12:04:17 +0800

    north_h

---
+++ Codeforces_-_SMU_Autumn_2023_Trial_4/A_Distances_to_Zero.cpp
@@ -0,0 +1,92 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-09-30 10:15:01
+ *
+ * Problem: A. Distances to Zero
+ * Contest: Codeforces - SMU Autumn 2023 Trial 4
+ * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/475589/problem/A
+ * MemoryL: 256 MB
+ * TimeL:   2000 ms
+ * ==================================================================================
+ */
+
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
+#define met_0(a) memset(a,0,sizeof a)
+#define met_1(a) memset(a,-1,sizeof a)
+#define met_x(a) memset(a,0x3f,sizeof a)
+#define mpy(a, b) memcopy(a,sizeof b,b)
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define PDD pair<double,double>
+#define PCI pair<char,int>
+#define PSI pair<string,int>
+#define ALL(a) a.begin(),a.end()
+#define rALL(a) a.rbegin(),a.rend()
+#define int128 __int128
+#define endl '\n'
+#define lcm(x,y) x*y/__gcd(x,y)
+#define debug1(a) cout << #a << '=' << a << endl;
+#define lf(x)   fixed << setprecision(x)
+const double PI = 3.1415926;
+const int N = 10010;
+const int M = 1910;
+const int MOD = 998244353;
+const double EPS = 1e-8;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), ans;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        if(a[i] == 0)ans.push_back(i);
+    }
+    if(a[0] != 0) {
+        for(int j = 0, k = ans[0]; j < ans[0]; j++, k--) {
+            a[j] = k;
+        }
+    }
+    sort(ALL(ans));
+    // for(auto i : ans)cout << i << ' ';
+    // cout << endl;
+    for(int i = 1; i < ans.size(); i++) {
+        int dis = ans[i] - ans[i - 1] - 1;
+        // debug1(dis);
+        // debug1(ans[i - 1]);
+        for(int j = ans[i - 1] + 1, k = 1; j <= ans[i - 1] + dis / 2; j++, k++) {
+            // debug1(j);
+            a[j] = k;
+        }
+        // debug1(k);
+        for(int j = ans[i] - 1, k = 1; j >= ans[i - 1] + dis / 2 + 1; j--, k++) {
+            a[j] = k;
+        }
+    }
+    if(a[n - 1] != 0) {
+        for(int j = ans[ans.size() - 1] + 1, k = 1; j < n; j++, k++) {
+            a[j] = k;
+        }
+    }
+    for(auto i : a)cout << i << ' ';
+    cout << endl;
+}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    // cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
---
+++ Codeforces_-_SMU_Autumn_2023_Trial_4/B_Shopping.cpp
@@ -0,0 +1,86 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-09-30 10:15:05
+ *
+ * Problem: B. Shopping
+ * Contest: Codeforces - SMU Autumn 2023 Trial 4
+ * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/475589/problem/B
+ * MemoryL: 256 MB
+ * TimeL:   1000 ms
+ * ==================================================================================
+ */
+
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
+#define met_0(a) memset(a,0,sizeof a)
+#define met_1(a) memset(a,-1,sizeof a)
+#define met_x(a) memset(a,0x3f,sizeof a)
+#define mpy(a, b) memcopy(a,sizeof b,b)
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define PDD pair<double,double>
+#define PCI pair<char,int>
+#define PSI pair<string,int>
+#define ALL(a) a.begin(),a.end()
+#define rALL(a) a.rbegin(),a.rend()
+#define int128 __int128
+#define endl '\n'
+#define lcm(x,y) x*y/__gcd(x,y)
+#define debug(a) cout << #a << '=' << a << endl;
+#define lf(x)   fixed << setprecision(x)
+const double PI = 3.1415926;
+const int N = 10010;
+const int M = 1910;
+const int MOD = 998244353;
+const double EPS = 1e-8;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {
+    int  n, m, k;
+    cin >> n >> m >> k;
+    // vector<int> pos(100);
+    vector<int> a(k);
+    map <int, int> mp;
+    for(int i = 0; i < k; i++) {
+        cin >> a[i];
+        mp[a[i]] = i;
+    }
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        set<int> st;
+        for(int j = 0, x; j < m; j++) {
+            cin >> x;
+            st.insert(x);
+            debug(mp[x]);
+            ans += mp[x] + 1;
+            mp[x] = 1;
+            int kk = st.size();
+            for(int l = 0; l < k; l++) {
+                if(!st.count(x)) {
+                    mp[a[l]] = kk;
+                    kk++;
+                }
+            }
+        }
+
+    }
+    cout << ans << endl;
+}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    // cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
