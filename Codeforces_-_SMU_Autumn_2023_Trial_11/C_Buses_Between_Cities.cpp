9b0a7f378dfe448c6732f5445de12d6b9bbc2f59
north_h <1635273401@qq.com>
2023-10-07 13:49:37 +0800

    north_h

--- Codeforces_-_SMU_Autumn_2023_Trial_11/C_Buses_Between_Cities.cpp
+++ Codeforces_-_SMU_Autumn_2023_Trial_11/C_Buses_Between_Cities.cpp
@@ -1,11 +1,11 @@
 /*
  * ==================================================================================
  * Author:  north_h
- * Time:    2023-10-05 10:26:49
+ * Time:    2023-10-07 13:12:04
  *
- * Problem: B. Suitable Replacement
- * Contest: Codeforces - SMU Autumn 2023 Trial 9
- * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/475596/problem/B
+ * Problem: C. Buses Between Cities
+ * Contest: Codeforces - SMU Autumn 2023 Trial 11
+ * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/475599/problem/C
  * MemoryL: 256 MB
  * TimeL:   1000 ms
  * ==================================================================================
@@ -15,7 +15,7 @@
 
 #include<bits/stdc++.h>
 
-#define IOS ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr);
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
 #define met_0(a) memset(a,0,sizeof a)
 #define met_1(a) memset(a,-1,sizeof a)
 #define met_x(a) memset(a,0x3f,sizeof a)
@@ -46,52 +46,12 @@ const int INF = 0x3f3f3f3f;
 
 using namespace std;
 
-void solve() {
-    string a, b;
-    cin >> a >> b;
-    map<char, int> pa, pb;
-    int sum = 0;
-    for(auto i : a) {
-        pa[i]++;
-        if(i == '?')sum++;
-    }
-    for(auto i : b) {
-        pb[i]++;
-    }
-    // for(auto [x, y] : pb)cout << x << ' ' << y << endl;
-    vector<char> ans;
-    // cout << endl;
-    // for(auto [x, y] : pa)cout << x << ' ' << y << endl;
-    // debug1(sum);
-    if(sum == 0) {
-        cout << a << endl;
-        return ;
-    }
-    while(sum) {
-        for(auto [x, y] : pb) {
-            if(pa[x] >= y)pa[x] -= y;
-            else {
-                int res = y - pa[x];
-                for(int i = 0; i < res; i++)ans.push_back(x);
-                sum -= res;
-                if(sum <= 0)break;
-            }
-        }
-    }
-    for(auto i : a) {
-        if(i != '?')cout << i;
-        else {
-            cout << ans.back();
-            ans.pop_back();
-        }
-    }
-    cout << endl;
-}
+void solve() {}
 
 int32_t main() {
     IOS;
     int h_h = 1;
-    // cin >> h_h;
+    cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
