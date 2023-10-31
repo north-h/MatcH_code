5c39bd70bb34149d2b97215841eb7938e168c442
north-h <1635273401@qq.com>
2023-10-31 23:11:20 +0800

    north_h

--- Codeforces_-_SMU_Autumn_2023_Round_5/G_Make_Them_Equal.cpp
+++ Codeforces_-_SMU_Autumn_2023_Round_5/G_Make_Them_Equal.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * ==================================================================================
  * Author:  north_h
  * Time:    2023-09-26 15:11:33
@@ -10,7 +10,7 @@
  * TimeL:   2000 ms
  * ==================================================================================
  */
- 
+
 #pragma GCC optimize("Ofast")
 
 #include<bits/stdc++.h>
@@ -20,7 +20,7 @@
 #define met_1(a) memset(a,-1,sizeof a)
 #define met_x(a) memset(a,0x3f,sizeof a)
 #define mpy(a, b) memcopy(a,sizeof b,b)
-#define ll long long
+#define int long long
 #define ld long double
 #define ull unsigned long long
 #define fi first
@@ -33,7 +33,7 @@
 #define rALL(a) a.rbegin(),a.rend()
 #define int128 __int128
 #define endl '\n'
-const int N = 10010;
+const int N = 1010;
 const int M = 1910;
 const int MOD = 98244353;
 const double EPS = 1e-8;
@@ -41,12 +41,46 @@ const int INF = 0x3f3f3f3f;
 
 using namespace std;
 
-void solve() {}
+int f[N];
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> b(n + 1), c(n + 1), dp(k + 1, 0);
+    int ans = 0;
+    for(int i = 1; i <= n; i++) {
+        cin >> b[i];
+        ans += f[b[i]];
+    }
+    int sum = 0;
+    for(int i = 1; i <= n; i++)cin >> c[i], sum += c[i];
+    if(ans <= k) {
+        cout << sum << endl;
+        return ;
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = k; j >= f[b[i]]; j--) {
+            dp[j] = max(dp[j], dp[j - f[b[i]]] + c[i]);
+        }
+    }
+    cout << dp[k] << endl;
+}
 
 int32_t main() {
+    met_x(f);
+    f[1] = 0;
+    for(int i = 1; i <= 1000; i++) {
+        for(int j = 1; j <= i; j++) {
+            f[i + i / j] = min(f[i + i / j], f[i] + 1);
+        }
+    }
+    // for(int i = 1; i <= 10; i++)cout << f[i] << ' ';
+    // cout << endl;
     IOS;
     int h_h = 1;
     cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
+
+
