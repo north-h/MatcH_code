d0a93daa0f92eee2534acfb913f9e0b793b442fc
north-h <1635273401@qq.com>
2023-11-09 01:09:09 +0800

    north_h

---
+++ Luogu/P3372_模板线段树_1.cpp
@@ -0,0 +1,70 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-11-09 00:57:54
+ *
+ * Problem: P3372 【模板】线段树 1
+ * Contest: Luogu
+ * URL:     https://www.luogu.com.cn/problem/P3372
+ * MemoryL: 125 MB
+ * TimeL:   1000 ms
+ * ==================================================================================
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N = 100010;
+
+int tr1[N], tr2[N];
+
+int lowbit(int x) {
+    return x & (-x);
+}
+
+void add(int x, int k) {
+    for(int i = x; i < N; i += lowbit(i)) {
+        tr1[i] += k;
+        tr2[i] += k * x;
+    }
+}
+
+void range_add(int l, int r, int x) {
+    add(l, x);
+    add(r + 1, x);
+}
+
+int query(int x) {
+    int res = 0;
+    for(int i = x; i; i -= lowbit(i)) {
+        res += (x + 1) * tr2[i] - tr1[i];
+    }
+    return res;
+}
+
+int range_query(int l, int r) {
+    return query(r) - query(l);
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1, x; i <= n; i++) {
+        cin >> x;
+        add(i, x);
+    }
+    while(m--) {
+        int op, l, r, k;
+        cin >> op;
+        if(op == 1) {
+            cin >> l >> r >> k;
+            range_add(l, r, k);
+        } else {
+            cin >> l >> r;
+            cout << range_query(l, r) << endl;
+        }
+    }
+    return 0;
+}
