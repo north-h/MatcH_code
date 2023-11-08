ca84d29e1f254df79bd34798a59a317e649d0c94
north-h <1635273401@qq.com>
2023-11-08 18:38:23 +0800

    north_h

---
+++ Codeforces_-_SMU_Autumn_2023_Trial_31/A_Vasya_and_Arrays.cpp
@@ -0,0 +1,101 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-11-08 12:11:17
+ *
+ * Problem: A. Vasya and Arrays
+ * Contest: Codeforces - SMU Autumn 2023 Trial 31
+ * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/484047/problem/A
+ * MemoryL: 256 MB
+ * TimeL:   1000 ms
+ * ==================================================================================
+ */
+
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
+#define met_0(a) memset(a,0,sizeof a)
+#define met_1(a) memset(a,-1,sizeof a)
+#define met_x(a) memset(a,0x3f,sizeof a)
+#define mpy(a, b) memcopy(a,sizeof b,b)
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define PDD pair<double,double>
+#define PCI pair<char,int>
+#define PSI pair<string,int>
+#define ALL(a) a.begin(),a.end()
+#define rALL(a) a.rbegin(),a.rend()
+#define int128 __int128
+#define endl '\n'
+#define lcm(x,y) x*y/__gcd(x,y)
+#define debug1(a) cout<<#a<<'='<<a<<endl
+#define debug2(a,b) cout<<#a<<'='<<a<<' '<<#b<<'='<<b<<endl
+#define lf(x)   fixed << setprecision(x)
+#define PI acos(-1)
+const int N = 10010;
+const int M = 1910;
+const int MOD = 998244353;
+const double EPS = 1e-8;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &i : a)cin >> i;
+    int m;
+    cin >> m;
+    vector<int> b(m);
+    for(auto &i : b)cin >> i;
+    reverse(ALL(a));
+    reverse(ALL(b));
+    ll sa = a.back(), sb = b.back();
+    a.pop_back();
+    b.pop_back();
+    int ans = 0;
+    while(a.size() && b.size()) {
+        if(sa == sb) {
+            ans++;
+            // if(a.size()) {
+            sa = a.back();
+            a.pop_back();
+            // }
+            // if(b.size()) {
+            sb = b.back();
+            b.pop_back();
+            // }
+        } else if(sa > sb) {
+            sb += b.back();
+            b.pop_back();
+        } else {
+            sa += a.back();
+            a.pop_back();
+        }
+    }
+    while(a.size()) {
+        sa += a.back();
+        a.pop_back();
+    }
+    while(b.size()) {
+        sb += b.back();
+        b.pop_back();
+    }
+    if(sa == sb)cout << ans + 1 << endl;
+    else cout << -1 << endl;
+}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    // cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
