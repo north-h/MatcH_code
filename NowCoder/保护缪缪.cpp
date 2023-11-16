e71f629001370c288e4a687c3bcbfb4909658e8d
north-h <1635273401@qq.com>
2023-11-16 19:07:31 +0800

    north_h

---
+++ NowCoder/保护缪缪.cpp
@@ -0,0 +1,74 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-11-16 17:02:13
+ *
+ * Problem: 保护缪缪
+ * Contest: NowCoder
+ * URL:     https://ac.nowcoder.com/acm/contest/69327/E
+ * MemoryL: 524288 MB
+ * TimeL:   4000 ms
+ * ==================================================================================
+ */
+
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
+#define met_0(a) memset(a,0,sizeof a)
+#define met_1(a) memset(a,-1,sizeof a)
+#define met_x(a) memset(a,0x3f,sizeof a)
+#define mpy(a, b) memcopy(a,sizeof b,b)
+#define int long long
+#define ld long double
+#define ull unsigned long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define PDD pair<double,double>
+#define PCI pair<char,int>
+#define PSI pair<string,int>
+#define ALL(a) a.begin(),a.end()
+#define rALL(a) a.rbegin(),a.rend()
+#define int128 __int128
+#define endl '\n'
+#define lcm(x,y) x*y/__gcd(x,y)
+#define debug1(a) cout<<#a<<'='<<a<<endl
+#define debug2(a,b) cout<<#a<<'='<<a<<' '<<#b<<'='<<b<<endl
+#define lf(x)   fixed << setprecision(x)
+#define PI acos(-1)
+const int N = 10010;
+const int M = 1910;
+const int MOD = 998244353;
+const double EPS = 1e-8;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    if(n == 1) {
+        cout << 0 << endl;
+        return ;
+    }
+    if(n == 2) {
+        cout << 1 << endl;
+        return ;
+    }
+    int ans = 0;
+    while(n) {
+        ans++;
+        n /= 2;
+    }
+    cout << ans *m << endl;
+}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
---
+++ NowCoder/安洁莉娜的能量计算.cpp
@@ -0,0 +1,74 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-11-16 17:13:43
+ *
+ * Problem: 安洁莉娜的能量计算
+ * Contest: NowCoder
+ * URL:     https://ac.nowcoder.com/acm/contest/69327/D
+ * MemoryL: 524288 MB
+ * TimeL:   4000 ms
+ * ==================================================================================
+ */
+
+#pragma GCC optimize("Ofast")
+
+#include<bits/stdc++.h>
+
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
+#define met_0(a) memset(a,0,sizeof a)
+#define met_1(a) memset(a,-1,sizeof a)
+#define met_x(a) memset(a,0x3f,sizeof a)
+#define mpy(a, b) memcopy(a,sizeof b,b)
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define PDD pair<double,double>
+#define PCI pair<char,int>
+#define PSI pair<string,int>
+#define ALL(a) a.begin(),a.end()
+#define rALL(a) a.rbegin(),a.rend()
+#define int128 __int128
+#define endl '\n'
+#define lcm(x,y) x*y/__gcd(x,y)
+#define debug1(a) cout<<#a<<'='<<a<<endl
+#define debug2(a,b) cout<<#a<<'='<<a<<' '<<#b<<'='<<b<<endl
+#define lf(x)   fixed << setprecision(x)
+#define PI acos(-1)
+const int N = 10010;
+const int M = 1910;
+const int MOD = 998244353;
+const double EPS = 1e-8;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    unordered_map<int, int> up;
+    for(int i = 0, x; i < n; i++) {
+        cin >> x;
+        up[x]++;
+    }
+    for(int i = 1; i < m; i++) {
+        if(up[i] % (i + 1) == 0) {
+            up[i + 1] = up[i] / (i + 1);
+        } else {
+            cout << "NO" << endl;
+            return ;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    // cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
