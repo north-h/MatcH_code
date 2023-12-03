09f09bb8eba0133eb478c6487d7598564bb2b5c0
north-h <1635273401@qq.com>
2023-12-03 11:28:05 +0800

    cf-2023-12-3-11:36

---
+++ AtCoder_-_Daiwa_Securities_Co_Ltd_Programming_Contest_2023AtCoder_Beginner_Contest_331/A_-_Tomorrow.cpp
@@ -0,0 +1,39 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-12-02 22:53:04
+ *
+ * Problem: A - Tomorrow
+ * Contest: AtCoder - Daiwa Securities Co. Ltd. Programming Contest 2023（AtCoder Beginner Contest 331）
+ * URL:     https://atcoder.jp/contests/abc331/tasks/abc331_a
+ * MemoryL: 1024 MB
+ * TimeL:   2000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr);
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define endl '\n'
+#define debug1(a) cout<<#a<<'='<<a<<endl
+#define debug2(a,b) cout<<#a<<'='<<a<<' '<<#b<<'='<<b<<endl
+#define lf(x)   fixed << setprecision(x)
+#define PI acos(-1)
+const int N = 10010;
+const int M = 110;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
--- Codeforces_-_Codeforces_Round_904_(Div_2)/C_Medium_Design.cpp
+++ Codeforces_-_Codeforces_Round_904_(Div_2)/C_Medium_Design.cpp
@@ -15,7 +15,7 @@
 #include<bits/stdc++.h>
 
 #define IOS ios::sync_with_stdio(false),cin.tie(nullptr);
-#define ll long long
+#define int long long
 #define fi first
 #define se second
 #define PII pair<int,int>
@@ -29,7 +29,55 @@ const int M = 110;
 
 using namespace std;
 
-void solve() {}
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<PII > seg;
+    vector<int> l, r;
+    for (int i = 1, x, y; i <= n; i++) {
+        cin >> x >> y;
+        seg.push_back({x, y});
+        if (x != 1) {
+            l.push_back(x);
+            l.push_back(y);
+        }
+        if (y != m) {
+            r.push_back(x);
+            r.push_back(y);
+        }
+    }
+    sort(l.begin(), l.end());
+    l.erase(unique(l.begin(), l.end()), l.end());
+    // debug1(l.size());
+    vector<int> s(n * 2 + 10);
+    for (int i = 0; i < n; i++) {
+        if (seg[i].fi == 1)continue;
+        auto ll = lower_bound(l.begin(), l.end(), seg[i].fi) - l.begin() + 1;
+        auto rr = lower_bound(l.begin(), l.end(), seg[i].se) - l.begin() + 1;
+        s[ll]++;
+        s[rr + 1]--;
+    }
+    int ans = 0;
+    for (int i = 1; i < s.size(); i++) {
+        s[i] += s[i - 1];
+        ans = max(ans, s[i]);
+    }
+    sort(r.begin(), r.end());
+    r.erase(unique(r.begin(), r.end()), r.end());
+    for(auto &i : s)i = 0;
+    for (int i = 0; i < n; i++) {
+        if (seg[i].se == m)continue;
+        auto ll = lower_bound(r.begin(), r.end(), seg[i].fi) - r.begin() + 1;
+        auto rr = lower_bound(r.begin(), r.end(), seg[i].se) - r.begin() + 1;
+        s[ll]++;
+        s[rr + 1]--;
+    }
+    for (int i = 1; i < s.size(); i++) {
+        s[i] += s[i - 1];
+        ans = max(ans, s[i]);
+    }
+    cout << ans << endl;
+}
 
 int32_t main() {
     IOS;
