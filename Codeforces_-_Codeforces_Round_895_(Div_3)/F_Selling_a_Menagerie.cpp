3bc2da650c2995089f98ce3cc3520a948a86756d
north-h <1635273401@qq.com>
2023-12-12 10:06:36 +0800

    cf

---
+++ Codeforces_-_Codeforces_Round_895_(Div_3)/F_Selling_a_Menagerie.cpp
@@ -0,0 +1,115 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-12-11 23:08:15
+ *
+ * Problem: F. Selling a Menagerie
+ * Contest: Codeforces - Codeforces Round 895 (Div. 3)
+ * URL:     https://codeforces.com/problemset/problem/1872/F
+ * MemoryL: 256 MB
+ * TimeL:   2000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr);
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define endl '\n'
+#define debug1(a) cout<<#a<<'='<<a<<endl
+#define debug2(a,b) cout<<#a<<'='<<a<<' '<<#b<<'='<<b<<endl
+#define lf(x)   fixed << setprecision(x)
+#define PI acos(-1)
+const int N = 10010;
+const int M = 0x3f3f3f3f;
+
+using namespace std;
+
+vector<int> g[N], a, c;
+int n, in[N], vis[N];
+vector<int> ans;
+
+void bfs() {
+    queue<int> q;
+    for(int i = 1; i <= n; i++) {
+        if(in[i] == 0) {
+            q.push(i);
+            vis[i] = 1;
+            ans.push_back(i);
+        }
+    }
+    while(q.size()) {
+        auto t = q.front();
+        q.pop();
+        for(auto i : g[t]) {
+            if(vis[i])continue;
+            in[i]--;
+            if(in[i] == 0) {
+                q.push(i);
+                vis[i] = 1;
+                ans.push_back(i);
+            }
+        }
+    }
+}
+
+void solve() {
+    cin >> n;
+    a.resize(n + 1, 0);
+    c.resize(n + 1, 0);
+    for(int i = 1; i <= n; i++) {
+        in[i] = 0;
+        g[i].clear();
+        vis[i] = 0;
+    }
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+        g[i].push_back(a[i]);
+        in[a[i]]++;
+    }
+    for(int i = 1; i <= n; i++)cin >> c[i];
+    ans.clear();
+    bfs();
+    for(int i = 1; i <= n; i++) {
+        vector<PII> ve;
+        if(!vis[i]) {
+            int now = a[i];
+            int mn = M;
+            int goal = i;;
+            while(now != i) {
+                if(mn > c[now]) {
+                    mn = c[now];
+                    goal = now;
+                }
+                now = a[now];
+            }
+            if(mn > c[now]) {
+                mn = c[now];
+                goal = now;
+            }
+            now = a[goal];
+            while(now != goal) {
+                vis[now] = 1;
+                ans.push_back(now);
+                now = a[now];
+            }
+            ans.push_back(now);
+            vis[now] = 1;
+        }
+    }
+    for(auto i : ans) {
+        cout << i << ' ';
+    }
+    cout << endl;
+}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
---
+++ Codeforces_-_Codeforces_Round_914_(Div_2)/F_Beautiful_Tree.cpp
@@ -0,0 +1,39 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-12-10 00:07:21
+ *
+ * Problem: F. Beautiful Tree
+ * Contest: Codeforces - Codeforces Round 914 (Div. 2)
+ * URL:     https://codeforces.com/contest/1904/problem/F
+ * MemoryL: 512 MB
+ * TimeL:   4000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr);
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define endl '\n'
+#define debug1(a) cout<<#a<<'='<<a<<endl
+#define debug2(a,b) cout<<#a<<'='<<a<<' '<<#b<<'='<<b<<endl
+#define lf(x)   fixed << setprecision(x)
+#define PI acos(-1)
+const int N = 10010;
+const int M = 110;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
