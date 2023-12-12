4cd5b8f58d89b0c55146fa3e500ee39d7ab2927e
north-h <1635273401@qq.com>
2023-12-13 00:28:23 +0800

    north_h

--- Codeforces_-_Codeforces_Round_897_(Div_2)/C_Salyg1n_and_the_MEX_Game.cpp
+++ Codeforces_-_Codeforces_Round_897_(Div_2)/C_Salyg1n_and_the_MEX_Game.cpp
@@ -15,35 +15,39 @@
 
 #include<bits/stdc++.h>
 
-#define IOS ios::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
-#define met_0(a) memset(a,0,sizeof a)
-#define met_1(a) memset(a,-1,sizeof a)
-#define met_x(a) memset(a,0x3f,sizeof a)
-#define mpy(a, b) memcopy(a,sizeof b,b)
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr);
 #define ll long long
-#define ld long double
-#define ull unsigned long long
 #define fi first
 #define se second
 #define PII pair<int,int>
-#define PDD pair<double,double>
-#define PCI pair<char,int>
-#define PSI pair<string,int>
-#define ALL(a) a.begin(),a.end()
-#define rALL(a) a.rbegin(),a.rend()
-#define int128 __int128
-#define endl '\n'
-#define lcm(x,y) x*y/__gcd(x,y)
+// #define endl '\n'
 const int N = 10010;
 const int M = 1910;
-const int MOD = 998244353;
-const double EPS = 1e-8;
 const int INF = 0x3f3f3f3f;
 
 using namespace std;
 
 void solve() {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    set<int> st;
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+        st.insert(a[i]);
+    }
+    int k = 0;
+    for(auto i : st) {
+        if(!st.count(k))break;
+        k++;
+    }
+    cout << k << endl;
+    for(int i = 1; i <= n * 2; i++) {
+        int x;
+        cin >> x;
+        if(x == -1)return ;
+        cout << x << endl;
+    }
 }
 
 int32_t main() {
---
+++ NowCoder/Problem_B_sophistry.cpp
@@ -0,0 +1,52 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2023-12-12 20:39:12
+ *
+ * Problem: Problem B. sophistry
+ * Contest: NowCoder
+ * URL:     https://ac.nowcoder.com/acm/contest/22754/B
+ * MemoryL: 524288 MB
+ * TimeL:   2000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#define IOS ios::sync_with_stdio(false),cin.tie(nullptr);
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int,int>
+#define endl '\n'
+#define debug1(a) cout<<#a<<'='<<a<<endl
+#define debug2(a,b) cout<<#a<<'='<<a<<' '<<#b<<'='<<b<<endl
+#define lf(x)   fixed << setprecision(x)
+#define PI acos(-1)
+const int N = 10010;
+const int M = 110;
+
+using namespace std;
+
+void solve() {
+    int n, d, m;
+    cin >> n >> d >> m;;
+    vector<ll> a(n + 1), dp(n + 2);
+    for(int i = 1; i <= n; i++)cin >> a[i];
+    for(int i = n; i >= 1; i--) {
+        if(a[i] > m) {
+            dp[i] = max(dp[i + 1], dp[min(i + d + 1, n + 1)] + a[i]);
+        } else dp[i] = dp[i + 1] + a[i];
+    }
+    // for(int i = 1; i <= n; i++)cout << dp[i] << ' ';
+    // cout << endl;
+    cout << dp[1] << endl;
+}
+
+int32_t main() {
+    IOS;
+    int h_h = 1;
+    // cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
