44b01a3821a124a4680f5e113fddf534164a1f23
north-h <1635273401@qq.com>
2023-12-21 02:13:13 +0800

    north_h

--- AcWing/验题.cpp
+++ AcWing/验题.cpp
@@ -16,22 +16,22 @@ const int M = 110;
 using namespace std;
 
 char g[N][N];
-int vis[N], dis[N];
-int n, m, ans = INT_MAX;
+int vis[N * N], dis[N * N];
+int n, m;
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
-int s, e;
+int s, e, x, y;
 vector<PII> G[N];
 map<PII, int> mp;
 
 int dijkstra(int x, int y) {
     memset(dis, 0x3f, sizeof dis);
-    dis[x *y = 0;
-          priority_queue<PII, vector<PII >, greater<PII>> heap;
-          heap.push({dis[mp[{x, y}]], mp[{x, y}]});
-      while (heap.size()) {
-          auto t = heap.top();
-          heap.pop();
+    dis[mp[ {x, y}]] = 0;
+    priority_queue<PII, vector<PII >, greater<PII>> heap;
+    heap.push({dis[mp[{x, y}]], mp[{x, y}]});
+    while (heap.size()) {
+        auto t = heap.top();
+        heap.pop();
         if (vis[t.se])continue;
         vis[t.se] = 1;
         for (auto [x, y] : G[t.se]) {
@@ -41,43 +41,37 @@ int dijkstra(int x, int y) {
             }
         }
     }
-    if (dis[s * e] == 0x3f3f3f3f)return -1;
-    return dis[s * e];
+    if (dis[mp[ {s, e}]] == 0x3f3f3f3f)return -1;
+    return dis[mp[ {s, e}]];
 }
 
 
 void solve() {
     cin >> n >> m;
-    int x, y;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             cin >> g[i][j];
-            mp[ {i, j}] = i * j;
+            mp[ {i, j}] = i * m + j + 1;
             if(g[i][j] == 'S')x = i, y = j;
             if(g[i][j] == 'T')s = i, e = j;
         }
     }
-    for(auto [x, y] : mp)cout << x.fi << ' ' << x.se << ' ' << y << endl;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(g[i][j] == 'K')continue;
             for(int k = 0; k < 4; k++) {
-                int x = i + dx[i];
-                int y = j + dy[i];
-                if(g[x][y] = '.') {
-                    G[i * j].emplace_back(x * y, 1);
+                int x = i + dx[k];
+                int y = j + dy[k];
+                if(x < 0 || y < 0 || x >= n || y >= m || g[x][y] == 'K')continue;
+                if(g[x][y] == '.' || g[x][y] == 'T' || g[x][y] == 'S') {
+                    G[mp[ {i, j}]].emplace_back(mp[ {x, y}], 1);
                 } else if(g[x][y] == 'A') {
-                    G[i * j].emplace_back(x * y, 5);
+                    G[mp[ {i, j}]].emplace_back(mp[ {x, y}], 4);
                 } else if(g[x][y] == '#') {
-                    G[i * j].emplace_back(x * y, 2);
+                    G[mp[ {i, j}]].emplace_back(mp[ {x, y}], 2);
                 }
             }
         }
-    }
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m; j++) {
-            cout << dis[mp[ {i, j}]] << ' ';
-        }
-        cout << endl;
     }
     cout << dijkstra(x, y) << endl;
 }
