b12e221b2734fabb6bc268333560d9a63a86e057
north_h <1635273401@qq.com>
2023-12-20 22:09:04 +0800

    yanti

--- AcWing/验题.cpp
+++ AcWing/验题.cpp
@@ -26,12 +26,12 @@ map<PII, int> mp;
 
 int dijkstra(int x, int y) {
     memset(dis, 0x3f, sizeof dis);
-    dis[mp[ {x, y}]] = 0;
-    priority_queue<PII, vector<PII >, greater<PII>> heap;
-    heap.push({dis[mp[{x, y}]], mp[{x, y}]});
-    while (heap.size()) {
-        auto t = heap.top();
-        heap.pop();
+    dis[x *y = 0;
+          priority_queue<PII, vector<PII >, greater<PII>> heap;
+          heap.push({dis[mp[{x, y}]], mp[{x, y}]});
+      while (heap.size()) {
+          auto t = heap.top();
+          heap.pop();
         if (vis[t.se])continue;
         vis[t.se] = 1;
         for (auto [x, y] : G[t.se]) {
@@ -41,8 +41,8 @@ int dijkstra(int x, int y) {
             }
         }
     }
-    if (dis[mp[ {s, e}]] == 0x3f3f3f3f)return -1;
-    return dis[mp[ {s, e}]];
+    if (dis[s * e] == 0x3f3f3f3f)return -1;
+    return dis[s * e];
 }
 
 
@@ -64,11 +64,11 @@ void solve() {
                 int x = i + dx[i];
                 int y = j + dy[i];
                 if(g[x][y] = '.') {
-                    G[mp[ {i, j}]].emplace_back(mp[ {x, y}], 1);
+                    G[i * j].emplace_back(x * y, 1);
                 } else if(g[x][y] == 'A') {
-                    G[mp[ {i, j}]].emplace_back(mp[ {x, y}], 5);
+                    G[i * j].emplace_back(x * y, 5);
                 } else if(g[x][y] == '#') {
-                    G[mp[ {i, j}]].emplace_back(mp[ {x, y}], 2);
+                    G[i * j].emplace_back(x * y, 2);
                 }
             }
         }
