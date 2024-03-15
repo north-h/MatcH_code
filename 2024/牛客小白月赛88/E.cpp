2a01d2d58765ae86565fd099c75f46a7202f62a9
north_h <1635273401@qq.com>
2024-03-15 21:07:10 +0800

    nowcoder

--- 2024/牛客小白月赛88/E.cpp
+++ 2024/牛客小白月赛88/E.cpp
@@ -15,64 +15,26 @@ const int INF = 0x3f3f3f3f;
 
 using namespace std;
 
-struct DSU {
-    vector<int> fa;
- 
-    DSU(int n) {
-        fa.resize(n + 1);
-        iota(fa.begin(), fa.end(), 0);
-        // for (int i = 1; i <= n; i ++) fa[i] = i;
-    }
- 
-    int find(int x) {
-        if (fa[x] != x) fa[x] = find(fa[x]);
-        return fa[x];
-    }
- 
-    bool same(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        return px == py;
-    }
- 
-    void merge(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if (px != py) {
-            fa[px] = py;
-        }
-    }
-};
-
 void solve() {
     int n, m;
     cin >> n >> m;
-    DSU dsu(N);
     vector<int> a(n + 1);
-    // for (auto i : a) cout << i << ' ';
-    for (int i = 1; i <= n; i ++) cin >> a[i];
+    unordered_map<int, int> fa;
+    for (int i = 1; i <= n; i ++) {
+        cin >> a[i];
+        fa[a[i]] = a[i];
+    }
     vector<PII> b(m + 1);
     for (int i = 1; i <= m; i ++) {
         cin >> b[i].fi >> b[i].se;
-        dsu.merge(b[i].fi, b[i].se);
     }
-    // for (int i = 1; i <= n; i ++) cout << dsu.find(i) << " \n"[i == n];
-    // debug1(dsu.same(1, 2));
-    // debug2(dsu.find(1), dsu.find(2));
-    set<int> st;
     for (int i = m; i >= 1; i --) {
-        if (st.count(dsu.find(b[i].se))) continue;
-        st.insert(dsu.find(b[i].fi));
-        st.insert(dsu.find(b[i].se));
-        for (int j = 1; j <= n; j ++) {
-            if (dsu.same(a[j], b[i].se)) {
-                // debug2(b[i].fi, b[i].se);
-                a[j] = b[i].se; 
-            }
-        }
+        int x = b[i].fi;
+        int y = b[i].se;
+        if (fa.count(fa[y])) fa[x] = fa[y];
+        else fa[x] = y;
     }
-    
-    for (int i = 1; i<= n; i++) cout << a[i] << " \n"[i == n];
+    for (int i = 1; i<= n; i++) cout << fa[a[i]] << " \n"[i == n];
 }
 
 int32_t main() {
