cada9f84c2e5e1f9f10639b27cc3b7a4ae28f127
north_h <1635273401@qq.com>
2024-03-18 11:09:19 +0800

    f

---
+++ 2024/牛客周赛 round 37/C.cpp
@@ -0,0 +1,30 @@
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int, int>
+#define endl '\n'
+#define debug1(a) cout << #a << '=' << a << endl
+#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define lf(x) fixed << setprecision(x)
+#define PI acos(-1)
+//  #define LOCAL
+const int N = 10010;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
