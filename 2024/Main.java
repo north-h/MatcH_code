af445f78bc852e8470382b11bb3438ef5970e385
north-h <1635273401@qq.com>
2024-03-19 11:42:50 +0800

    north_h

---
+++ 2024/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        var a = sc.nextInt();
+        var b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
--- 2024/田地.py
+++ 2024/田地.py
@@ -1,9 +1,5 @@
-x, y, z = map(float, input().split())
-a = x * y / z + x * z / y + y * z / x
-b = (x + y + z)
-if a == b:
-    print('=')
-elif a < b:
-    print('<')
-else:
-    print('>')
+print("hello world")
+
+a = 1
+b = 1
+print(a + b)
