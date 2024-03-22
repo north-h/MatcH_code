831e2028975bef40036fc4c7499549408c157d66
north-h <1635273401@qq.com>
2024-03-23 01:51:46 +0800

    codeforces

---
+++ 2024/Codeforces Round 936 (Div. 2)/A.cpp
@@ -0,0 +1,46 @@
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int, int>
+#define endl '\n'
+#define debug1(a) cout << #a << '=' << a << endl
+#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define lf(x) fixed << setprecision(x)
+#define PI acos(-1)
+//  #define LOCAL
+const int N = 10010;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    map<int, int> mp;
+    for (int i = 1; i <= n; i ++) {
+        cin >> a[i];
+        mp[a[i]] ++;
+    }
+    sort(a.begin() + 1, a.end());
+    int x = (n + 1) / 2;
+    int ans = 0;
+    for (int i = x; i <= n; i ++) {
+        if (a[i] == a[x]) ans ++;
+    }
+    cout << ans << endl;
+}
+
+int32_t main() {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
