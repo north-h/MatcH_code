837e25533a31dc452a461acb9b7baf622afb00eb
north-h <1635273401@qq.com>
2024-03-27 01:23:44 +0800

    luogu

---
+++ 2024/Luogu/P3372_模板线段树_1.cpp
@@ -0,0 +1,43 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2024-03-27 01:14:19
+ *
+ * Problem: P3372 【模板】线段树 1
+ * Contest: Luogu
+ * URL:     https://www.luogu.com.cn/problem/P3372
+ * MemoryL: 125 MB
+ * TimeL:   1000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int, int>
+#define endl '\n'
+#define debug1(a) cout << #a << '=' << a << endl
+#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define lf(x) fixed << setprecision(x)
+#define PI acos(-1)
+//  #define LOCAL
+const int N = 10010;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
