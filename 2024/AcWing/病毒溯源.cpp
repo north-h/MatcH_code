888d02f2eb89718f84ac3a180076b71123f98ff9
north-h <1635273401@qq.com>
2024-04-02 00:51:11 +0800

    init

---
+++ 2024/AcWing/病毒溯源.cpp
@@ -0,0 +1,43 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2024-04-02 00:43:14
+ *
+ * Problem: 病毒溯源
+ * Contest: AcWing
+ * URL:     https://www.acwing.com/problem/content/3468/
+ * MemoryL: 64 MB
+ * TimeL:   4000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define PII pair<int, int>
+#define endl '\n'
+#define debug1(a) cout << #a << '=' << a << endl
+#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define lf(x) fixed << setprecision(x)
+#define PI acos(-1)
+// #define LOCAL
+const int N = 10010;
+const int INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
