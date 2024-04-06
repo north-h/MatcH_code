78720f17e9bd992c4f600b3b768612d2803cb107
north_h <1635273401@qq.com>
2024-04-06 20:26:22 +0800

    north_h

--- 2024/田地.cpp
+++ 2024/田地.cpp
@@ -1,7 +1,4 @@
-// #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
-#define fi first;
-#define se second;
 #define debug1(a) cout << #a << '=' << a << endl
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
 #define lf(x) fixed << setprecision(x)
@@ -11,7 +8,7 @@ const int INF = 0x3f3f3f3f;
 
 using namespace std;
 using ll = long long;
-using PII =  pair<int, int>;
+using i2 = array<int, 2>;
 
 void solve() {}
 
@@ -22,7 +19,7 @@ int32_t main() {
 #endif
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
-    cin >> h_h;
+    // cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
