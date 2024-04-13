2a779bae20b19cbc09a609de34a6b80064d8edcd
north-h <1635273401@qq.com>
2024-04-13 22:13:27 +0800

    north_h

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -1,69 +1,67 @@
-/*
- * ==================================================================================
- * Author:  north_h
- * Time:    2024-03-19 00:45:42
- *
- * Problem: P1879 [USACO06NOV] Corn Fields G
- * Contest: Luogu
- * URL:     https://www.luogu.com.cn/problem/P1879
- * MemoryL: 125 MB
- * TimeL:   1000 ms
- * ==================================================================================
- */
-
 // #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
-#define ll long long
-#define fi first
-#define se second
-#define PII pair<int, int>
-#define endl '\n'
+#define fi first;
+#define se second;
 #define debug1(a) cout << #a << '=' << a << endl
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
 #define lf(x) fixed << setprecision(x)
-#define PI acos(-1)
-//  #define LOCAL
-const int N = 10010;
+// #define LOCAL
+const int N = 1000010;
 const int INF = 0x3f3f3f3f;
-const int mod = 1e8;
 
 using namespace std;
+using ll = long long;
+
+char g[1010][1010], ans[1010][1010];
+int n, m;
+int dx[8] = {1, 1, 0, 0, -1, -1, 1, -1};
+int dy[8] = {1, -1, 1, -1, 1, -1, 0, 0};
+
 
 void solve() {
-    int n, m;
     cin >> n >> m;
-    vector<vector<int>> a(n + 1, vector<int>(m + 1));
-    vector<int> sum(n + 1);
     for (int i = 1; i <= n; i ++) {
         for (int j = 1; j <= m; j ++) {
-            cin >> a[i][j];
-            sum[i] = (sum[i] << 1) + a[i][j];
+            cin >> g[i][j];
+            ans[i][j] = '#';
         }
     }
-    vector<int> p((1<<m) + 1);
-    for (int i = 0; i < 1<<m; i ++) {
-        int x = (i & (i << 1));
-        int y = (i & (i >> 1));
-        if (x == 0 && y == 0) p[i] = 1;
-    }
-    vector<vector<ll>> dp(n + 1, vector<ll>(1<<m + 1));
-    dp[0][0] = 1;
     for (int i = 1; i <= n; i ++) {
-        for (int j = 0; j < 1<<m; j ++) {
-            if (!p[j]) continue;
-            if ((j & sum[i]) != j) continue;
-            for (int k = 0; k < 1<<m; k ++) {
-                if ((k & j)) continue;
-                dp[i][j] = dp[i][j] + dp[i - 1][k];
-                dp[i][j] %= mod;
+        for (int j = 1; j <= m; j ++) {
+            if (ans[i][j] != '#') continue;
+            if (g[i][j] == 'M') ans[i][j] = g[i][j];
+            else if (g[i][j] == 'H') {
+                for (int k = 0; k < 8; k ++) {
+                    int x = dx[k] + j; int y = dy[k] + j;
+                    if (x < 1 || y < 1 || x > n || y > m) continue;
+                    ans[x][y] = 'G';
+                }
+            }
+            else {
+                int c = 0, v = 0;
+                for (int k = 0; k < 8; k ++) {
+                    int x = dx[k] + j; int y = dy[k] + j;
+                    if (x < 1 || y < 1 || x > n || y > m) continue;
+                    if (g[x][y] == 'L') c ++;
+                    if (g[x][y] == 'H') v ++;
+                    if (g[x][y] != 'H' && g[x][y] != 'M') ans[x][y] = 'N';
+                }
+                // debug2(i, j);
+                // debug2(c, v);
+                if (g[i][j] == 'L') {
+                    if (c == 8) ans[i][j] = 'S';
+                    else ans[i][j] = 'B';
+                } else ans[i][j] = 'B';
             }
         }
     }
-    ll ans = 0;
-    for (int i = 0; i < 1<<m; i ++) {
-        ans = (ans + dp[n][i]) % mod;
+//     debug2(n, m);
+    for (int i = 1; i <= n; i ++) {
+        for (int j = 1; j <= m; j ++) {
+            cout << ans[i][j];
+        }
+        cout << '\n';
     }
-    cout << ans << endl;
 }
 
 int32_t main() {
@@ -73,7 +71,7 @@ int32_t main() {
 #endif
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
-    // cin >> h_h;
+//    cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
