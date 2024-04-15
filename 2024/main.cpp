7cf4cf9f94682368f9f72c7799cd5fec9ba80c3a
north_h <1635273401@qq.com>
2024-04-15 10:53:28 +0800

    north_h

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -11,53 +11,9 @@ using namespace std;
 using ll = long long;
 
 void solve() {
-    string s;
-    cin >> s;
-    string sans;
-    int ans = 0, cnt = 0, f = 0;
-    for (int i = 0; i < s.size(); i ++) {
-        if (s[i] == '$') break;
-        if (s[i] == 'T') {
-            cnt ++;
-            ans += 10;
-            sans += 'D';
-            if (!f) {
-                sans += '-';
-                sans += '-';
-                i += 2;
-            } else {
-                f --;
-                if (f == 0) {
-                    sans += '-';
-                    sans += '-';
-                    i += 2;
-                }
-            }
-        } else if (s[i] == 'C') {
-            cnt ++;
-            sans += '!';
-            f += 2;
-
-        
-            if (f == 0) f += 2;
-
-        } else {
-            if (!f) {
-                sans += 'U';
-                sans += '-';
-                i ++;
-            } else {
-                f --;
-                if (f == 0) {
-                    sans += 'U';
-                    sans += '-';
-                    i ++;
-                }
-            }
-        }
-    }
-    cout << sans.substr(0, s.size() - 1) << '\n';
-    cout << ans - cnt * 3 << '\n';
+    int a, b, c, d;
+    cin >> a >>  b >> c >> d;
+    
 }
 
 int32_t main() {
