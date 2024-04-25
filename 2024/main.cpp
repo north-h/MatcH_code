bec2197eb6f90058989537656fa2a22001eec4d6
north-h <1635273401@qq.com>
2024-04-26 00:02:39 +0800

    north_h

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -4,32 +4,13 @@
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
 #define lf(x) fixed << setprecision(x)
 // #define LOCAL
-const int N = 210;
+const int N = 100010;
 const int INF = 0x3f3f3f3f;
 
 using namespace std;
 using ll = long long;
 
-void solve() {
-    int n; cin >> n;
-    map<int, int> mp;
-    vector<int> a(n + 1);
-    for (int i = 1; i <= n; i ++) {
-        cin >> a[i];
-    }
-    int ans = 0;
-    auto b = a;
-    sort(a.begin() + 1, a.end());
-    for (int i = 1; i <= n; i ++) {
-        for (int j = 1; j <= n; j ++) {
-            if (a[i] == b[j]) {
-                ans += abs(i - j);
-                break;
-            }
-        }
-    }
-    cout << ans << '\n';
-}
+void solve() {}
 
 int32_t main() {
 #ifdef LOCAL
