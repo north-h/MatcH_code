88311cfbd9c03fd5d727b1719bbc461dc2d3638a
north-h <1635273401@qq.com>
2024-05-03 13:04:43 +0800

    codeforces

--- 2024/Codeforces_-_Codeforces_Round_943_(Div_3)/D_Permutation_Game.cpp
+++ 2024/Codeforces_-_Codeforces_Round_943_(Div_3)/D_Permutation_Game.cpp
@@ -17,42 +17,56 @@
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
 #define lf(x) fixed << setprecision(x)
 // #define LOCAL
+#define int long long
 const int N = 100010;
 const int INF = 0x3f3f3f3f;
 
 using namespace std;
-using ll = long long;
+// using ll = long long;
 
 void solve() {
     int n, k, pb, ps;
     cin >> n >> k >> pb >> ps;
     vector<int> p(n + 1), a(n + 1);
+    int mx = 0;
     for (int i = 1; i <= n; i ++) cin >> p[i];
-    for (int i = 1; i <= n; i ++) cin >> a[i];
-    int ss = 0, sb = 0, c = 0;
-    bool fs = 1, fb = 1;
-    while (true) {
-        if (c % 2 == 0) {
-            sb += a[pb];
-            if (fb && a[p[pb]] > a[pb] && ps != p[pb]) pb = p[pb];
-            else fb = 0;
-        } else {
-            ss += a[ps];
-            if (fs && a[p[ps]] > a[ps] && pb != p[ps]) ps = p[ps];
-            else fs = 0;
-        }
-        c ++;
-        k --;
-        if (!fb && !fs) {
-            sb += (k + 1) / 2 * a[pb];
-            ss += k / 2 * a[ps];
-            break;
-        }
+    for (int i = 1; i <= n; i ++) {
+        cin >> a[i];
+        mx = max(a[i], mx);
     }
-    debug2(pb, ps);
-    debug2(sb, ss);
+    int ss = 0, sb = 0, mxb = 0, mxs = 0, ys = 0, yb = 0;
+    for (int i = 1, c = 0; i <= min(k, n); i ++) {
+        c += a[pb];
+        int x = c + (min(k, n) - i) * a[pb];
+        // debug1(x);
+        if (a[pb] == mx) yb = c + (k - i) * mx;
+        if (sb < x) sb = x, mxb = a[pb];
+        pb = p[pb];
+    }
+    for (int i = 1, c = 0; i <= min(k, n); i ++) {
+        c += a[ps];
+        int x = c + (min(k, n) - i) * a[ps];
+        // debug2(x, c);
+        // debug2(a[ps], s);
+        if (a[ps] == mx) ys = c + (k - i) * mx;
+        if (ss < x) ss = x, mxs = a[ps];
+        ps = p[ps];
+    }
+    // if (mxb == b) lb = 0;
+    // if (mxs == s) ls = 0;
+    // debug2(lb, ls);
+    // debug2(yb, ys);
+    // debug2(sb, ss);
+    // debug2(mxb, mxs);
+    if (k > n) {
+        sb += (k - n) * mxb;
+        ss += (k - n) * mxs;
+    }
+    sb = max(sb, yb);
+    ss = max(ss, ys);
+    // debug2(sb, ss);
     if (ss == sb) cout << "Draw" << '\n';
-    else if (sb > ss) cout << "Body" << '\n';
+    else if (sb > ss) cout << "Bodya" << '\n';
     else cout << "Sasha" << '\n';
 }
 
