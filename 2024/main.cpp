249090f1c6d2894cbe23fbeb0501086febace9b3
north-h <1635273401@qq.com>
2024-05-11 01:54:41 +0800

    codeforces

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -4,15 +4,46 @@
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
 #define lf(x) fixed << setprecision(x)
 // #define LOCAL
+#define int long long
 const int N = 100010;
 const int INF = 0x3f3f3f3f;
-const int mod = 1e9 + 7;
 
 using namespace std;
 using ll = long long;
 
 void solve() {
-    int n, m; cin >> n >> m;
+    int n; cin >> n;
+    if (n == 1) {
+        cout << 8 << '\n';
+        return ;
+    }
+    if (n == 2) {
+        cout << 16 << '\n';
+        return ;
+    }
+    int l = 0, r = n, ans = 0;
+    auto check = [&](int x) -> bool {
+        int y = x * x + n * n;
+        return y < (n + 1) * (n + 1);
+    };
+    while (l <= r) {
+        int mid = l + r >> 1;
+        if (check(mid)) l = mid + 1, ans = mid;
+        else r = mid - 1;
+    }
+    debug1(ans);
+    int c = ceil(n * 1.0 / sqrt(2));
+    l = ceil(n * 1.0 / sqrt(2)), r = n;
+    int res = -1;
+    while (l <= r) {
+        int mid = l + r >> 1;
+        if (mid * sqrt(2) < n + 1) l = mid + 1, res = mid;
+        else r = mid - 1;
+    }
+    debug2(res, c);
+    int v = ans * 2;
+    if (res != -1) v += (res - c + 1);
+    cout << v * 4 + 4 << '\n';
 }
 
 int32_t main() {
@@ -22,7 +53,7 @@ int32_t main() {
 #endif
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
-    // cin >> h_h;
+    cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
--- 2024/田地.cpp
+++ 2024/田地.cpp
@@ -1,192 +1,54 @@
-#include<iostream>
-#include<cstring>
-#include<vector>
-#include<numeric>
-#include<functional>
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define debug1(a) cout << #a << '=' << a << endl
+#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define lf(x) fixed << setprecision(x)
+// #define LOCAL
+#define PII pair<int, int>
+#define int long long
+const int N = 100010;
+const int INF = 0x3f3f3f3f;
+
 using namespace std;
-using LL = long long;
+// using ll = long long;
 
-const LL INF = 1e18;
-struct Info{
-    LL mx = -INF, mn = INF;
-    int mnpos = -1;
-    LL ans1 = -INF, ans2 = -INF;
-};
-
-using Tag = LL;
-
-Info operator+(const Info &a, const Info &b){
-    Info ret = Info();
-    ret.mx = max(a.mx, b.mx);
-    ret.mn = min(a.mn, b.mn);
-    if (a.mn < b.mn) ret.mnpos = a.mnpos;
-    else ret.mnpos = b.mnpos;
-    ret.ans1 = max(a.ans1, b.ans1);
-    ret.ans1 = max(ret.ans1, b.mx * 2 - a.mn);
-    ret.ans2 = max(a.ans2, b.ans2);
-    ret.ans2 = max(ret.ans2, a.mx * 2 - b.mn);
-    return ret;
+void solve() {
+    int n, k, q;
+    cin >> n >> k >> q;
+    vector<int> a(k + 1), b(k + 1);
+    for (int i = 1; i <= k; i ++) cin >> a[i];
+    for (int i = 1; i <= k; i ++) cin >> b[i];
+    vector<int> c(1);
+    map<int, double> mp;
+    for (int i = 1; i <= k; i ++) {
+        int x = a[i] - a[i - 1], y = b[i] - b[i - 1];
+        c.push_back(a[i - 1] + 1);
+        mp[a[i - 1] + 1] = x * 1.0 / y;
+    }
+    while (q --) {
+        int x; cin >> x;
+        if (x == 0) { cout << 0 << ' '; continue; }
+        int l = 1, r = k, p = -1;
+        while (l <= r) {
+            int mid = l + r >> 1;
+            if (x >= c[mid]) l = mid + 1, p = mid;
+            else r = mid - 1;
+        }
+        // debug1(p);
+        int ans = b[p - 1] + (x - c[p] + 1) * 1.0 / mp[c[p]];
+        cout << lf(0) << floor(ans) << ' ';
+    }
+    cout << '\n';
 }
 
-void apply(Info &x, Tag &a, Tag f){
-    x.mx += f;
-    x.mn += f;
-    x.ans1 += f;
-    x.ans2 += f;
-    a += f;
-}
-
-template<class Info, class Tag>
-struct LazySegmentTree{
-    int n;
-    vector<Info> info;
-    vector<Tag> tag;
-
-    LazySegmentTree() {}
-
-    LazySegmentTree(int n, Info _init = Info()){
-        init(vector<Info>(n, _init));
-    }
-
-    LazySegmentTree(const vector<Info> &_init){
-        init(_init);
-    }
-
-    void init(const vector<Info> &_init){
-        n = (int)_init.size();
-        info.assign((n << 2) + 1, Info());
-        tag.assign((n << 2) + 1, Tag());
-        function<void(int, int, int)> build = [&](int p, int l, int r){
-            if (l == r){
-                info[p] = _init[l];
-                return;
-            }
-            int m = (l + r) / 2;
-            build(2 * p, l, m);
-            build(2 * p + 1, m + 1, r);
-            pull(p);
-        };
-        build(1, 0, n - 1);
-    }
-    
-    void pull(int p){
-        info[p] = info[2 * p] + info[2 * p + 1];
-    }
-    
-    void apply(int p, const Tag &v){
-        ::apply(info[p], tag[p], v);
-    }
-    
-    void push(int p){
-        apply(2 * p, tag[p]);
-        apply(2 * p + 1, tag[p]);
-        tag[p] = Tag();
-    }
-    
-    void modify(int p, int l, int r, int x, const Info &v){
-        if (l == r){
-            info[p] = v;
-            return;
-        }
-        int m = (l + r) / 2;
-        push(p);
-        if (x <= m){
-            modify(2 * p, l, m, x, v);
-        } 
-        else{
-            modify(2 * p + 1, m + 1, r, x, v);
-        }
-        pull(p);
-    }
-    
-    void modify(int p, const Info &v){
-        modify(1, 0, n - 1, p, v);
-    }
-    
-    Info query(int p, int l, int r, int x, int y){
-        if (l > y || r < x){
-            return Info();
-        }
-        if (l >= x && r <= y){
-            return info[p];
-        }
-        int m = (l + r) / 2;
-        push(p);
-        return query(2 * p, l, m, x, y) + query(2 * p + 1, m + 1, r, x, y);
-    }
-    
-    Info query(int l, int r){
-        return query(1, 0, n - 1, l, r);
-    }
-    
-    void modify(int p, int l, int r, int x, int y, const Tag &v){
-        if (l > y || r < x){
-            return;
-        }
-        if (l >= x && r <= y){
-            apply(p, v);
-            return;
-        }
-        int m = (l + r) / 2;
-        push(p);
-        modify(2 * p, l, m, x, y, v);
-        modify(2 * p + 1, m + 1, r, x, y, v);
-        pull(p);
-    }
-    
-    void modify(int l, int r, const Tag &v){
-        return modify(1, 0, n - 1, l, r, v);
-    }
-};
-
-int main(){
-
+int32_t main() {
 #ifdef LOCAL
     freopen("data.in", "r", stdin);
     freopen("data.out", "w", stdout);
 #endif
-
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    int T;
-    cin >> T;
-    while(T--){
-        int n, q;
-        cin >> n >> q;
-        vector<LL> s(n + 1), a(n + 1);
-        vector<Info> init(n + 1);
-        for(int i = 1; i <= n; i++){
-            cin >> a[i];
-            s[i] = s[i - 1] + a[i];
-        }
-        for(int i = 0; i <= n; i++){
-            init[i] = {s[i], s[i], i, -INF, -INF};
-        }
-        LazySegmentTree<Info, Tag> seg(init);
-    
-        while(q--){
-            int op, x, y;
-            cin >> op >> x >> y;
-            if (op == 1){
-                seg.modify(x, n, y - a[x]);
-                a[x] = y;
-            }
-            else{
-                auto res = seg.query(x - 1, y);
-                LL mx1 = seg.query(res.mnpos + 1, y).ans2;
-                LL mx2 = seg.query(x - 1, res.mnpos - 1).ans1;
-                LL ans = max(mx1, mx2) - res.mn;
-                {
-                    LL t = 2 * res.mn;
-                    t -= seg.query(x - 1, res.mnpos - 1).mn;
-                    t -= seg.query(res.mnpos + 1, y).mn;
-                    ans = max(ans, t);
-                }
-                cout << ans << '\n';
-            }
-        }
-    }
-
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
 }
