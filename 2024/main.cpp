ce6805c06a12b76b94e485cc170ab31fd0e55e22
north-h <1635273401@qq.com>
2024-05-16 13:25:18 +0800

    SMU

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -10,10 +10,42 @@ const int INF = 0x3f3f3f3f;
 using namespace std;
 using ll = long long;
 
+int n;
+int dp[N], a[N];
+vector<int> g[N];
+
+void dfs1(int u, int f) {
+    for (auto v : g[u]) {
+        if (v == f) continue;
+        if (a[v] * 2 >= a[u]) dp[v] ++;
+        dfs1(v, u);
+        dp[u] += dp[v];
+    }
+}
+
+void dfs2(int u, int f) {
+    for (auto v : g[u]) {
+        if (v == f) continue;
+        dp[v] += dp[u] - dp[v] - 1;
+        if (a[u] * 2 >= a[v]) dp[v] ++;
+        dfs2(v, u);
+    }
+}
+
 void solve() {
-    ll n, d; cin >> n >> d;
-    ll ans = 1234567890 + d;
-    cout << ans * n * 10 << '\n';
+    cin >> n;
+    for (int i = 1; i <= n; i ++) cin >> a[i];
+    for (int i = 1; i < n; i ++) {
+        int u, v; cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    dfs1(1, 0);
+    for (int i = 1; i <= n; i ++) cout << dp[i] << " \n"[i == n];
+    dfs2(1, 0);
+    int ans = 0;
+    for (int i = 1; i <= n; i ++) ans += (dp[i] == n - 1);
+    cout << ans << '\n';
 }
 
 int32_t main() {
