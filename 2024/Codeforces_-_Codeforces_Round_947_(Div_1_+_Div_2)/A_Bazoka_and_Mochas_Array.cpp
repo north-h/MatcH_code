d804cd46c73b49f43f089c3fee2a9a3115ec4bb8
north-h <1635273401@qq.com>
2024-05-26 02:38:47 +0800

    codeforces

--- 2024/Codeforces_-_Codeforces_Round_947_(Div_1_+_Div_2)/A_Bazoka_and_Mochas_Array.cpp
+++ 2024/Codeforces_-_Codeforces_Round_947_(Div_1_+_Div_2)/A_Bazoka_and_Mochas_Array.cpp
@@ -23,7 +23,17 @@ const int INF = 0x3f3f3f3f;
 using namespace std;
 using ll = long long;
 
-void solve() {}
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i ++) cin >> a[i];
+    int cv = 0;
+    for (int i = 2; i <= n; i ++) {
+        if (a[i - 1] > a[i]) cv ++;
+    }
+    if (cv == 1 && a[1] >= a[n] || cv == 0) cout << "Yes" << '\n';
+    else cout << "No" << '\n';
+}
 
 int32_t main() {
 #ifdef LOCAL
