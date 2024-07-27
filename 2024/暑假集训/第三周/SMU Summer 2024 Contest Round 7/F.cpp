d1edc4a960680e419c452510215685e05704bdcc
north-h <1635273401@qq.com>
2024-07-27 23:55:34 +0800

    north_h

--- 2024/暑假集训/第三周/SMU Summer 2024 Contest Round 7/F.cpp
+++ 2024/暑假集训/第三周/SMU Summer 2024 Contest Round 7/F.cpp
@@ -3,6 +3,7 @@
 #define debug1(a) cout << #a << '=' << a << endl
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
 #define lf(x) fixed << setprecision(x)
+// #define LOCAL
 #define int long long
 const int N = 100010;
 const int INF = 0x3f3f3f3f;
@@ -13,11 +14,38 @@ void solve() {
     int n, m; cin >> n >> m;
     map<int, vector<array<int, 2>>> mp;
     for (int i = 1; i <= m; i ++) {
-        
+        int x, y, z; cin >> x >> y >> z;
+        mp[x].push_back({y, z});
     }
+    vector<int> dp(1 << n);
+    dp[0] = 1;
+    for (int i = 1; i < 1 << n; i ++) {
+        vector<int> t;
+        int cv = 0, ok = 0;
+        for (int j = 0; j < n; j ++) {
+            if (i >> j & 1) t.push_back(j + 1), cv ++;
+        }
+        for (auto [y, z] : mp[cv]) {
+            int cnt = 0;
+            for (auto j : t) {
+                if (j <= y) cnt ++;
+                if (cnt > z) ok = 1;
+            }
+        }
+        if (!ok) {
+            for (auto j : t) {
+                dp[i] += dp[i ^ (1 << j - 1)];
+            }
+        }
+    }
+    cout << dp[(1 << n) - 1] << '\n';
 }
 
 int32_t main() {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
     // cin >> h_h;
