2a5c4b504c0274137f1a3226584a68928193a981
north_h <1635273401@qq.com>
2024-09-02 16:42:14 +0800

    north_h

--- 2024/CT.cpp
+++ 2024/CT.cpp
@@ -5,7 +5,6 @@
 #define debug1(a) cout << #a << '=' << a << endl
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
 #define lf(x) fixed << setprecision(x)
-#define LOCAL
 #define int long long
 const int N = 100010;
 const int INF = 0x3f3f3f3f;
@@ -14,83 +13,10 @@ using namespace std;
 
 void solve() {
     int n, k; cin >> n >> k;
-    vector<int> p(n + 1), c(n + 1);
-    int ans = LLONG_MIN;
-    for (int i = 1; i <= n; i ++) cin >> p[i];
-    for (int i = 1; i <= n; i ++) {
-        cin >> c[i];
-        ans = max(ans, c[i]);
-    }
-    vector<int> vis(n + 1);
-    map<int, vector<int>> mp;
-    int idx = 1;
-    for (int i = 1; i <= n; i ++) {
-        if (vis[i]) continue;
-        int j = i;
-        mp[idx].push_back(0);
-        while (!vis[j]) {
-            vis[j] = 1;
-            j = p[j];
-            mp[idx].push_back(c[j]);
-        }
-        idx ++;
-    }
-    // for (auto &[x, y] : mp) {
-    //     cout << x << ':';
-    //     for (auto j : y) cout << j << ' ';
-    //     cout << '\n';
-    // }
-    map<int, int> up;
-    for (auto &[x, y] : mp) {
-        // cout << x << ':';
-        int m = y.size();
-        up[x] = y.size() - 1;
-        for (int j = 1; j < m; j ++) y.push_back(y[j]);
-        for (int j = 1; j < (int)y.size(); j ++) y[j] += y[j - 1];
-        // for (auto j : y) cout << j << ' ';
-        // cout << '\n';
-    }
-    for (auto [x, y] : mp) {
-        int mn = up[x];
-        for (int len = 1; len <= min(mn - 1, k); len ++) {
-            for (int j = 1; j + len - 1 < (int)y.size(); j ++) {
-                int val = y[j + len - 1] - y[j - 1];
-                ans = max(val, ans);
-            }
-        }
-        // debug2(ans, mn);
-        if (k >= mn) {
-            int c1 = y[mn], c2 = LLONG_MIN;
-            int v1 = k / mn, v2 = k % mn;
-            for (int j = 1; j + v2 - 1 < (int)y.size(); j ++) {
-                c2 = max(c2, y[j + v2 - 1] - y[j - 1]);
-            }
-            int T = 0;
-            for (int len = 1; len <= mn; len ++) {
-                for (int j = 1; j + len - 1 < (int)y.size(); j ++) {
-                    int vl = y[j + len - 1] - y[j - 1];
-                    int vi = y[mn] - vl;
-                    // debug2(vi, vl);
-                    T = max(T, max(vi, vl));
-                }
-            }
-            // debug2(c1, c2);
-            // debug2(v1, v2);
-            // debug1(T);
-            int res;
-            if (c2 == 0 && v1 > 1) res = c1 * (v1 - 1) + T;
-            else res = c1 * v1 + c2;
-            ans = max(ans, res);
-        }
-    }
-    cout << ans << '\n';
+    cout << k << '\n';
 }
 
 int32_t main() {
-#ifdef LOCAL
-    freopen("data.in", "r", stdin);
-    freopen("data.out", "w", stdout);
-#endif
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
     // cin >> h_h;
--- 2024/main.cpp
+++ 2024/main.cpp
@@ -2,105 +2,30 @@
 #include <bits/stdc++.h>
 #define debug1(a) cout << #a << '=' << a << endl
 #define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define lf(x) fixed << setprecision(x)
 #define int long long
-#define i128 __int128
-const i128 N = 1e15;
+const int N = 100010;
 const int INF = 0x3f3f3f3f;
 
 using namespace std;
 
-void write(i128 x) {
-    if (x < 0) putchar('-'), x = -x;
-    if (x > 9) write(x / 10);
-    putchar(x % 10 + '0');
-}
-
-int n;
-string L, R;
-i128 cnt1, cnt2, cnt3, cnt4, cnt5, cnt6;
-i128 num1, num2, num3, num4;
-
-i128 check1(i128 l, i128 r, i128 xx) {
-    i128 res = 0, lg = l, rg = r;
-    while (l <= r) {
-        i128 mid = (l + r) / 2;
-        if (mid * mid >= xx) {
-            res = mid;
-            r = mid - 1;
-        } else l = mid + 1;
-    }
-    return res;
-}
-
-i128 check2(i128 l, i128 r, i128 xx) {
-    i128 res = 0, lg = l, rg = r;
-    while (l <= r) {
-        i128 mid = (l + r) / 2;
-        i128 m = mid;
-        if (mid * mid <= xx) {
-            res = mid;
-            l = mid + 1;
-        } else r = mid - 1;
-    }
-    return res;
-}
-
 void solve() {
-    cin >> n >> L >> R;
-    for (int i = 0; i < n / 2; i++) {
-        num1 = num1 * 10 + L[i] - '0';
-        num3 = num3 * 10 + R[i] - '0';
-    }
-    for (int i = n / 2; i < n; i++) {
-        num2 = num2 * 10 + L[i] - '0';
-        num4 = num4 * 10 + R[i] - '0';
-    }
-
-    i128 ans = 0;
-
-    i128 l = 0, r = num3 - 1;
-    r = min(r, N);
-    cnt1 = check1(l, r, num1 + 1);
-    cnt2 = check2(l, r, num3 - 1);
-    r = 9;
-    for (int i = 1; i < n / 2; i++) r = r * 10 + 9;
-    i128 s9 = r;
-    r = min(r, N);
-    cnt3 = check2(l, r, s9); ////通用情况
-    if (num3 - num1 > 1) {
-        ans += (cnt2 - cnt1 + 1) * (cnt3 + 1);
-    }
-
-    l = 0, r = num1;
-    r = min(r, N);
-    i128 b = check2(l, r, num1);
-    if (b * b == num1) {
-        l = 0;
-        if (num1 == num3) r = num4;
-        else r = s9;
-        r = min(r, N);
-        cnt4 = check1(l, r, num2);
-        if (num1 == num3) cnt5 = check2(l, r, num4);
-        else cnt5 = check2(l, r, s9);
-        ans += (cnt5 - cnt4 + 1);
-    }    ////左固定
-
-    l = 0, r = num3;
-    r = min(r, N);
-    i128 c = check2(l, r, num3);
-    if (c * c == num3) {
-        l = 0, r = num4;
-        r = min(r, N);
-        cnt6 = check2(l, r, num4);
-        if (num1 != num3) ans += (cnt6 + 1);
-    }    ////右固定
-    write(ans);
+     int l, r; cin >> l >> r;
+     int x = r - l;
+    //  debug1(x);
+     int L = 1, R = 1e9, ans = 0;
+     while (L <= R) {
+        int mid = L + R >> 1;
+        if ((mid + 1) * mid / 2 <= x) L = mid + 1, ans = mid;
+        else R = mid - 1;
+     }
+     cout << ans + 1 << '\n';
 }
 
 int32_t main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
-    // cin >> h_h;
+    cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
