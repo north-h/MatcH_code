7000b854d25fd865d53742a6ae7ed824e73bec64
north-h <1635273401@qq.com>
2024-09-09 20:47:48 +0800

    north_h

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -1,31 +1,35 @@
-// #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
-#define debug1(a) cout << #a << '=' << a << endl
-#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
-#define lf(x) fixed << setprecision(x)
 #define int long long
-const int N = 100010;
-const int INF = 0x3f3f3f3f;
-
+#define pii pair<int, int>
+#define ll long long
 using namespace std;
-
-void solve() {
-     int l, r; cin >> l >> r;
-     int x = r - l;
-    //  debug1(x);
-     int L = 1, R = 1e9, ans = 0;
-     while (L <= R) {
-        int mid = L + R >> 1;
-        if ((mid + 1) * mid / 2 <= x) L = mid + 1, ans = mid;
-        else R = mid - 1;
-     }
-     cout << ans + 1 << '\n';
-}
-
-int32_t main() {
-    ios::sync_with_stdio(false), cin.tie(nullptr);
-    int h_h = 1;
-    cin >> h_h;
-    while (h_h--)solve();
+const int N = 109;
+const int mod = 998244353;
+int dp[N][N][N], n, m;
+string S, T;
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    cin >> S >> T;
+    n = S.length();
+    m = T.length();
+    S = ' ' + S;
+    T = ' ' + T;
+    for (int i = 0; i <= n; i++)
+        for (int j = 1; j <= m + 1; j++)
+            for (int t = 0; t < j; t++)
+                dp[i][j][t] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int l = 1; l <= m; l++)
+            for (int r = l; r <= m; r++) {
+                for (int k = l - 1; k <= r; k++)
+                    dp[i][l][r] = (dp[i][l][r] + dp[i - 1][l][k] * dp[i - 1][k + 1][r]) % mod;
+                for (int k = l - 1; k < r; k++)
+                    if (S[i] == T[k + 1])
+                        dp[i][l][r] = (dp[i][l][r] + dp[i - 1][l][k] * dp[i - 1][k + 2][r]) % mod;
+            }
+    }
+    cout << dp[n][1][m] << '\n';
+    //system("pause");  // 交之前记得注释一下，关流的时候不会显示输出
     return 0;
 }
