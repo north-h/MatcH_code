f0041448c76ace52419c36645dfccc7992face6e
north-h <1635273401@qq.com>
2024-09-10 00:20:24 +0800

    north_h

--- 2024/Codeforces_-_SMU_Autumn_2024_Trial_1/A_Load_Balancing.cpp
+++ 2024/Codeforces_-_SMU_Autumn_2024_Trial_1/A_Load_Balancing.cpp
@@ -20,12 +20,63 @@ const int N = 100010, INF = 0x3f3f3f3f;
 
 using namespace std;
 
-void solve() {}
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n + 1);
+    int sum = 0;
+    for (int i = 1; i <= n; i ++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    sort(a.begin() + 1, a.end());
+    int ans = 0;
+    int lt = sum / n, rt = lt + 1, cl, cr;
+    if (lt) {
+        if (sum % n) cr = sum % n, cl = n - cr;
+        else cl = n, cr = n, rt = lt;;
+    } else {
+        int cc = 0;
+        for (int i = 1; i <= n; i ++) {
+            if (a[i] != 0) cc ++;
+        }
+        cout << sum - cc << '\n';
+        return;
+    }
+    int ok = 0;
+    for (int i = 1, j = n; i < j;) {
+        int vx = abs(a[i] - lt), vy = abs(a[j] - rt);
+        if (vx < vy) {
+            ans += vx;
+            a[j] -= vx;
+            a[i] += vx;
+            i ++; 
+            if (ok == 0) cl --;
+            else cr --, cl --;
+        } else if (vx > vy) {
+            ans += vy;
+            a[j] -= vy;
+            a[i] += vy;
+            j --;
+            if (ok == 0) cr --;
+            else cl --, cr --;
+        } else {
+            ans += vx;
+            a[i] += vx;
+            a[j] -= vx;
+            i ++, j --;
+            if (ok == 0) cl --, cr --;
+            else cl -= 2, cr -= 2;
+        }
+        if (cl == 0) lt = rt, ok ++;
+        else if (cr == 0) rt = lt, ok ++;
+    }
+    cout << ans << '\n';
+}
 
 int32_t main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
-    cin >> h_h;
+    // cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
