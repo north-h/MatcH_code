1fab3d21546f99dcc1e0d338a2586dde269685a5
north_h <1635273401@qq.com>
2024-09-21 11:32:57 +0800

    north_h

---
+++ 2024/Codeforces_-_SMU_Autumn_2024_Trial_8/B_Zmei_Gorynich.cpp
@@ -0,0 +1,31 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2024-09-21 10:53:22
+ *
+ * Problem: B. Zmei Gorynich
+ * Contest: Codeforces - SMU Autumn 2024 Trial 8
+ * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/551418/problem/B
+ * MemoryL: 256 MB
+ * TimeL:   1000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define debug1(a) cout << #a << '=' << a << endl
+#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define int long long
+const int N = 100010, INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
---
+++ 2024/Codeforces_-_SMU_Autumn_2024_Trial_8/C_Sorting_By_Multiplication.cpp
@@ -0,0 +1,31 @@
+/*
+ * ==================================================================================
+ * Author:  north_h
+ * Time:    2024-09-21 10:53:24
+ *
+ * Problem: C. Sorting By Multiplication
+ * Contest: Codeforces - SMU Autumn 2024 Trial 8
+ * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/551418/problem/C
+ * MemoryL: 256 MB
+ * TimeL:   2000 ms
+ * ==================================================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define debug1(a) cout << #a << '=' << a << endl
+#define debug2(a, b) cout << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define int long long
+const int N = 100010, INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
