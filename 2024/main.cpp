8632549aae86c43c0ee56d75d47f8e4495180829
north-h <1635273401@qq.com>
2024-10-02 13:02:17 +0800

    north_h

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -20,67 +20,26 @@ const int N = 100010, INF = 0x3f3f3f3f;
 
 using namespace std;
 
+int n, k;
+
 void solve() {
-    int n; cin >> n;
-    vector<int> a(n + 1);
-    map<int, int> mp;
-    for (int i = 1; i <= n; i ++) cin >> a[i], mp[a[i]] ++;
-    sort(a.begin() + 1, a.end());
-    // for (int i = 1; i <= n; i ++) {
-    //     cout << a[i] << ' ';
-    // }
-    // cout << '\n';
-    map<int, int> vis;
-    auto dfs = [&](auto dfs, int x) -> bool {
-        if (mp[x]) {
-            vis[x] ++;
-            mp[x] --;
-            return true;
-        }
-        if (x % 2 == 0 || x == 1) return false;
-        bool ok = dfs(dfs, x / 2);
-        ok &= dfs(dfs, (x + 1) / 2);
-        return ok;
-    };
-    vector<int> ans;
-    for (int i = n; i >= 1; i --) {
-        if (mp[a[i]] == 0) continue;
-        // debug1(a[i]);
-        // for (auto [x, y] : mp) {
-        //     debug2(x, y);
-        // }
-        bool ok = false;
-        while (dfs(dfs, a[i] * 2 + 1)) {
-            ans.push_back(a[i] * 2 + 1);
-            // ok = true;
-            // debug1(a[i] * 2 + 1);
-            vis.clear();
-        }
-        for (auto [x, y] : vis) mp[x] += y;
-        vis.clear();
-        while (dfs(dfs, a[i] * 2 - 1)) {
-            ans.push_back(a[i] * 2 - 1);
-            vis.clear();
-            // debug1(a[i] * 2 - 1);
-        }
-        for (auto [x, y] : vis) mp[x] += y;
-        // if (mp[a[i]] && !ok) ans.push_back(a[i]), mp[a[i]] --;
-        vis.clear();
-        // cout << '\n';
+    cin >> n >> k;
+    if (n < k || k == 1) {cout << n << '\n';return ;}
+    if (n == k) {cout << 1 << '\n';return ;}
+    int ans = 0;
+    while (n) {
+        int sum = 1;
+        while (sum * k <= n) sum *= k;
+        ans += n / sum;
+        n -= n / sum * sum;
     }
-    for (int i = 1; i <= n; i ++) {
-        if (mp[a[i]]) ans.push_back(a[i]), mp[a[i]] --;;
-    }
-    sort(ans.rbegin(), ans.rend());
-    cout << ans.size() << '\n';
-    for (auto i : ans) cout << i << ' ';
-    cout << '\n';
+    cout << ans << '\n';
 }
 
 int32_t main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
-    // cin >> h_h;
+    cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
