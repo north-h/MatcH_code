a1ab5055a38e8215a1cbc1f8801ae6bfac22feeb
north_h <1635273401@qq.com>
2024-10-10 20:00:29 +0800

    north_h

--- 2024/Luogu/Little_Pony_and_Expected_Maximum.cpp
+++ 2024/Luogu/Little_Pony_and_Expected_Maximum.cpp
@@ -1,12 +1,12 @@
 /*
  * ==============================================================
  * Author:  north_h
- * Time:    2024-10-09 22:07:46
+ * Time:    2024-10-10 18:52:58
  *
- * Problem: P3802 小魔女帕琪
+ * Problem: Little Pony and Expected Maximum
  * Contest: Luogu
- * URL:     https://www.luogu.com.cn/problem/P3802
- * MemoryL: 125 MB
+ * URL:     https://www.luogu.com.cn/problem/CF453A
+ * MemoryL: 250 MB
  * TimeL:   1000 ms
  * ==============================================================
  */
@@ -21,12 +21,18 @@ const int N = 100010, INF = 0x3f3f3f3f;
 using namespace std;
 
 void solve() {
+    int n, m; cin >> m >> n;
+    double ans = 0;
+    for (int i = 1; i <= m; i ++) {
+        ans += i * (pow(i * 1.0 / m, n) - pow((i - 1) * 1.0 / m, n));
+    }
+    cout << fixed << setprecision(4) << ans << '\n';
 }
 
 int32_t main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int h_h = 1;
-    cin >> h_h;
+    // cin >> h_h;
     while (h_h--)solve();
     return 0;
 }
