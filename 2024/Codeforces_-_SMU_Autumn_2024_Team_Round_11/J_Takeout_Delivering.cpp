bbc55b68923c08239fa2a2a68fce1d27de6ac68c
north_h <1635273401@qq.com>
2024-11-02 17:46:06 +0800

    north_h

---
+++ 2024/Codeforces_-_SMU_Autumn_2024_Team_Round_11/J_Takeout_Delivering.cpp
@@ -0,0 +1,31 @@
+/*
+ * ==============================================================
+ * Author:  north_h
+ * Time:    2024-11-02 12:43:31
+ *
+ * Problem: J. Takeout Delivering
+ * Contest: Codeforces - SMU Autumn 2024 Team Round 11
+ * URL:     https://codeforces.com/group/L9GOcnr1dm/contest/563273/problem/J
+ * MemoryL: 128 MB
+ * TimeL:   2000 ms
+ * ==============================================================
+ */
+
+// #pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define debug1(a) cerr << #a << '=' << a << endl
+#define debug2(a, b) cerr << #a << '=' << a << ' ' << #b << '=' << b << endl
+#define int long long
+const int N = 100010, INF = 0x3f3f3f3f;
+
+using namespace std;
+
+void solve() {}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+    int h_h = 1;
+    cin >> h_h;
+    while (h_h--)solve();
+    return 0;
+}
